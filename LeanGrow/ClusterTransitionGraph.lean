f17972dae6c546f7414e419a4c475e27989a4773
Happyves <yves-j@gmx.de>
2024-07-29 16:12:51 +0200

    nrjgwrgöj

--- LeanGrow/ClusterTransitionGraph.lean
+++ LeanGrow/ClusterTransitionGraph.lean
@@ -198,9 +198,9 @@ elab "makeProcessedTransitionGraph" : command => do
       if hits = 0
       then A := A.push []
       else
-        dbg_trace s!"{common}"
-        common := common.qsort (fun (_,v1) (_,v2) => v1 ≥ v2)
-        dbg_trace s!"▸          {((List.range 10).map (common.get!))}"
+        --dbg_trace s!"{common}"
+        common := common.qsort (fun (_,v1) (_,v2) => v1 > v2)
+        --dbg_trace s!"▸          {((List.range 10).map (common.get!))}"
         let mut S := 0
         let mut s := 0
         for (_,v) in common do
@@ -223,8 +223,8 @@ elab "makeProcessedTransitionGraph" : command => do
     if hits = 0
     then A := A.push []
     else
-      common := common.qsort (fun (_,v1) (_,v2) => v1 ≥ v2)
-      dbg_trace s!"{((List.range 10).map (common.get!))}"
+      common := common.qsort (fun (_,v1) (_,v2) => v1 > v2)
+      --dbg_trace s!"{((List.range 10).map (common.get!))}"
       let mut S := 0
       let mut s := 0
       for (_,v) in common do
@@ -245,12 +245,133 @@ elab "makeProcessedTransitionGraph" : command => do
 
 --makeProcessedTransitionGraph
 
-#eval cl_L_a.size
+elab "makeProcessedTransitionGraph2" : command => do
+  let mut a_o_as := #[]
+  let split_param := 10
+  let num_splits := cl_L_a.size / split_param
+  for ca in [0:(num_splits)] do
+    let mut A := #[]
+    for spc in [0:(split_param)] do
+      let (_,clu) := cl_L_a.get! (split_param*ca + spc)
+      let mut common := (Array.range (cl_L_a.size)).map (fun x => (x,0))
+      let mut hits := true
+      for pd in clu do
+        for mc in [0:cl_L_a.size] do
+          if Trie.CountCommon pd.goal_cst_names (cl_L_a.get! mc).1 ≠ 0
+          then common := common.modify mc (fun (x,n) => (x, Nat.succ n)) ; hits := false
+          else continue
+      -- take clusters until common-count drops by half
+      if hits
+      then A := A.push []
+      else
+        common := common.qsort (fun (_,v1) (_,v2) => v1 > v2)
+        let mut S := 0
+        let mut s := (common.get! 0).2
+        for x in [1 : common.size] do
+          let temp := common.get! x
+          if temp.2 ≥ (s / 2) ∧ temp.2 ≠ 0
+          then S := S+1 ; s := temp.2
+          else break
+        A := A.push ((List.range S).map (common.get!))
+    a_o_as := a_o_as.push A
+  -- last bit
+  let mut A := #[]
+  for spc in [0:(cl_L_a.size  % split_param)] do
+    let (_,clu) := cl_L_a.get! (split_param*num_splits + spc)
+    let mut common := (Array.range (cl_L_a.size)).map (fun x => (x,0))
+    let mut hits := 0
+    for pd in clu do
+      for mc in [0:cl_L_a.size] do
+        if Trie.CountCommon pd.goal_cst_names (cl_L_a.get! mc).1 ≠ 0
+        then common := common.modify mc (fun (x,n) => (x, Nat.succ n)) ; hits := hits + 1
+        else continue
+    if hits = 0
+    then A := A.push []
+    else
+      common := common.qsort (fun (_,v1) (_,v2) => v1 > v2)
+      let mut S := 0
+      let mut s := (common.get! 0).2
+      for x in [1 : common.size] do
+        let temp := common.get! x
+        if temp.2 ≥ (s / 2) ∧ temp.2 ≠ 0
+        then S := S+1 ; s := temp.2
+        else break
+      A := A.push ((List.range S).map (common.get!))
+  a_o_as := a_o_as.push A
+  -- print
+  let mut toSource := []
+  let mut co := 0
+  for a in a_o_as do
+    toSource := s!"\ndef cl_L_a_t_{co} : Array (List (Nat × Nat)) := {a}" :: toSource
+    co := co+1
+  toSource := toSource.reverse
+  let source := String.join toSource
+  IO.FS.writeFile ⟨"/./home/yves/Desktop/CodeWorkspace/Lean4_General/LeanGrow/LeanGrow/Caches/ProcessedTransitionGraphs.lean"⟩ (source)
 
-#eval Array.qsort #[(1,0),(2,2),(3,1),(4,0),(4,4),(5,5),(6,0),(7,10),(8,70),(9,0),(10,3)] (fun (_,v1) (_,v2) => v1 ≥ v2)
+--makeProcessedTransitionGraph2
 
-def testin := #[(0, 0), (1, 2), (2, 0), (3, 0), (4, 2), (5, 2), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0), (12, 2), (13, 0), (14, 0), (15, 0), (16, 0), (17, 0), (18, 0), (19, 0), (20, 2), (21, 0), (22, 0), (23, 2), (24, 0), (25, 0), (26, 0), (27, 2), (28, 0), (29, 2), (30, 0), (31, 0), (32, 2), (33, 0), (34, 0), (35, 0), (36, 0), (37, 0), (38, 0), (39, 0), (40, 0), (41, 0), (42, 0), (43, 0), (44, 0), (45, 0), (46, 2), (47, 0), (48, 0), (49, 0), (50, 0), (51, 0), (52, 0), (53, 2), (54, 0), (55, 0), (56, 2), (57, 2), (58, 0), (59, 0), (60, 0), (61, 0), (62, 0), (63, 2), (64, 0), (65, 0), (66, 0), (67, 0), (68, 0), (69, 0), (70, 0), (71, 0), (72, 2), (73, 0), (74, 0), (75, 0), (76, 2), (77, 2), (78, 0), (79, 0), (80, 2), (81, 0), (82, 2), (83, 0), (84, 0), (85, 0), (86, 0), (87, 0), (88, 2), (89, 0), (90, 2), (91, 0), (92, 0), (93, 0), (94, 2), (95, 0), (96, 0), (97, 0), (98, 0), (99, 0), (100, 2), (101, 0), (102, 2), (103, 2), (104, 0), (105, 0), (106, 0), (107, 0), (108, 0), (109, 0), (110, 0), (111, 2), (112, 0), (113, 0), (114, 0), (115, 0), (116, 0), (117, 0), (118, 0), (119, 0), (120, 0), (121, 0), (122, 0), (123, 0), (124, 0), (125, 0), (126, 2), (127, 0), (128, 0), (129, 2), (130, 2), (131, 2), (132, 2), (133, 2), (134, 2), (135, 0), (136, 0), (137, 0), (138, 0), (139, 0), (140, 0), (141, 0), (142, 0), (143, 0), (144, 0), (145, 0), (146, 0), (147, 0), (148, 0), (149, 0), (150, 2), (151, 2), (152, 0), (153, 0), (154, 0), (155, 0), (156, 0), (157, 0), (158, 0), (159, 0), (160, 0), (161, 0), (162, 0), (163, 0), (164, 0), (165, 0), (166, 0)]
 
-#eval testin.size
+elab "makeProcessedTransitionGraphGoals" : command => do
+  let mut a_o_as := #[]
+  let split_param := 10
+  let num_splits := cl_L_a.size / split_param
+  for ca in [0:(num_splits)] do
+    let mut A := #[]
+    for spc in [0:(split_param)] do
+      let (_,clu) := cl_L_a.get! (split_param*ca + spc)
+      let mut common := (Array.range (g_cl_L_a.size)).map (fun x => (x,0))
+      let mut hits := true
+      for pd in clu do
+        for mc in [0:g_cl_L_a.size] do
+          if Trie.CountCommon pd.goal_cst_names (g_cl_L_a.get! mc).1 ≠ 0
+          then common := common.modify mc (fun (x,n) => (x, Nat.succ n)) ; hits := false
+          else continue
+      -- take clusters until common-count drops by half
+      if hits
+      then A := A.push []
+      else
+        common := common.qsort (fun (_,v1) (_,v2) => v1 > v2)
+        let mut S := 0
+        let mut s := (common.get! 0).2
+        for x in [1 : common.size] do
+          let temp := common.get! x
+          if temp.2 ≥ (s / 2) ∧ temp.2 ≠ 0
+          then S := S+1 ; s := temp.2
+          else break
+        A := A.push ((List.range S).map (common.get!))
+    a_o_as := a_o_as.push A
+  -- last bit
+  let mut A := #[]
+  for spc in [0:(cl_L_a.size  % split_param)] do
+    let (_,clu) := cl_L_a.get! (split_param*num_splits + spc)
+    let mut common := (Array.range (g_cl_L_a.size)).map (fun x => (x,0))
+    let mut hits := 0
+    for pd in clu do
+      for mc in [0:g_cl_L_a.size] do
+        if Trie.CountCommon pd.goal_cst_names (g_cl_L_a.get! mc).1 ≠ 0
+        then common := common.modify mc (fun (x,n) => (x, Nat.succ n)) ; hits := hits + 1
+        else continue
+    if hits = 0
+    then A := A.push []
+    else
+      common := common.qsort (fun (_,v1) (_,v2) => v1 > v2)
+      let mut S := 0
+      let mut s := (common.get! 0).2
+      for x in [1 : common.size] do
+        let temp := common.get! x
+        if temp.2 ≥ (s / 2) ∧ temp.2 ≠ 0
+        then S := S+1 ; s := temp.2
+        else break
+      A := A.push ((List.range S).map (common.get!))
+  a_o_as := a_o_as.push A
+  -- print
+  let mut toSource := []
+  let mut co := 0
+  for a in a_o_as do
+    toSource := s!"\ndef g_cl_L_a_t_{co} : Array (List (Nat × Nat)) := {a}" :: toSource
+    co := co+1
+  toSource := toSource.reverse
+  let source := String.join toSource
+  IO.FS.writeFile ⟨"/./home/yves/Desktop/CodeWorkspace/Lean4_General/LeanGrow/LeanGrow/Caches/ProcessedTransitionGraphsGolas.lean"⟩ (source)
 
-#eval Array.qsort testin (fun (_,v1) (_,v2) => v1 ≥ v2)
+--makeProcessedTransitionGraphGoals
--- LeanGrow/WorkOnTransitionGraph.lean
+++ LeanGrow/WorkOnTransitionGraph.lean
@@ -1,2 +1,27 @@
 
-import LeanGrow.Caches.transitionGraphs
+import LeanGrow.Caches.ProcessedTransitionGraphs
+import LeanGrow.Caches.ProcessedTransitionGraphsGolas
+import LeanGrow.LtxManagement.Iffs
+import LeanGrow.LtxManagement.TryInduction
+import LeanGrow.PathCounter
+
+open Lean
+
+
+def randSelectCluster (data : List (Nat × Nat)) : IO Nat := do
+  let (cumulate, count) : (List (Nat × Nat)) × Nat := (data.foldl (fun (C,c) (clu,tran) => ((clu, c + tran):: C, c + tran)) ([],0))
+  let cumul' := cumulate.reverse
+  let r ← IO.rand 0 count
+  let mut ret := 0
+  for (cl,c) in cumul' do
+    if c ≥ r then ret := cl ; break
+  return ret
+
+
+-- make array of arrays via ++, also in source ?
+elab "makePathCountGraph" : command => do
+  let res := QT_build 0 (cl_L_a.size - 1) 0 (g_cl_L_a.size - 1) (weighted_walk_counter cl_L_a g_cl_L_a) 3
+  let source := s!"import LeanGrow.Quadtree\n" ++ (String.intercalate "\n" (res.map (fun (n,qt) => s!"def {n} : QT Nat Nat Wrap := {QT.toString instToStringNat.toString instToStringNat.toString ValPost.toStringTrick qt}")))
+  IO.FS.writeFile ⟨"/./home/yves/Desktop/CodeWorkspace/Lean4_General/LeanGrow/LeanGrow/Caches/PathCountGraph.lean"⟩ (source)
+
+--makePathCountGraph
