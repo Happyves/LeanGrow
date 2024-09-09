5118125c64cae1024ba57f9cf5a1f79f5afe4d50
Happyves <yves-j@gmx.de>
2024-09-09 17:55:18 +0200

    fpiwrgnprgnwpr

--- LeanGrow/EmpiricalScore.lean
+++ LeanGrow/EmpiricalScore.lean
@@ -64,7 +64,7 @@ def Expr.getFunBody : Expr → Expr
 
 open Meta
 
-partial def count_props : Expr → MetaM Nat
+partial def count_thms : Expr → MetaM Nat
 | .lam n t b _ => do
     let fvarId ← mkFreshFVarId
     let ctx ← read
@@ -74,23 +74,24 @@ partial def count_props : Expr → MetaM Nat
       (if
         let .some c := (← isClass? t)
         then
-        withNewLocalInstance c fvar <| count_props (b.instantiate1 fvar)
+        withNewLocalInstance c fvar <| count_thms (b.instantiate1 fvar)
         else
-        count_props (b.instantiate1 fvar)
+        count_thms (b.instantiate1 fvar)
           )
-| .mdata _ e | .proj _ _ e => count_props e
+| .mdata _ e | .proj _ _ e => count_thms e
 | .app l r => do
-      let cl ← count_props l
-      let cr ← count_props r
+      let cl ← count_thms l
+      let cr ← count_thms r
       return cl+cr
-| e => do --const and fvar
-      if (← inferType e).isProp then return 1 else return 0
+| .const c l => do --const and fvar
+      if (← inferType (.const c l)).isProp then return 1 else return 0
+| _ => return 0
 
 
 
-partial def sample_goal_and_unrelated_hyps (proof : Expr) : MetaM (Option (Expr × List Expr)) :=
+partial def sample_goal_and_hyps (proof : Expr) : MetaM (Option (Name × (Expr × Context) × List (Expr × Context))) :=
   match proof with
-  | .lam n t b i => do
+  | .lam n t b _ => do
       let fvarId ← mkFreshFVarId
       let ctx ← read
       let lctx := ctx.lctx.mkLocalDecl fvarId n t
@@ -99,19 +100,64 @@ partial def sample_goal_and_unrelated_hyps (proof : Expr) : MetaM (Option (Expr 
         (if
           let .some c := (← isClass? t)
          then
-          withNewLocalInstance c fvar <| sample_goal_and_unrelated_hyps (b.instantiate1 fvar)
+          withNewLocalInstance c fvar ((Option.map (fun (s,g,h) => (s,g, (t,ctx) :: h))) <$> (sample_goal_and_hyps (b.instantiate1 fvar)))
          else
-          sample_goal_and_unrelated_hyps (b.instantiate1 fvar)
+          ((Option.map (fun (s,g,h) => (s,g, (t,ctx) :: h))) <$> (sample_goal_and_hyps (b.instantiate1 fvar)))
            )
-  | .mdata _ e | .proj _ _ e => sample_goal_and_unrelated_hyps e
-  | _ => do
-      let args := proof.getAppArgs
-      let h := proof.getAppFn
-      if (← inferType h).isProp
-      then
-        match h with
-        | .const n _ =>
+  | .mdata _ e | .proj _ _ e => sample_goal_and_hyps e
+  | e => do
+      let args := e.getAppArgs
+      let h := e.getAppFn
+      match h with
+      | .const n _ =>
+            if (← inferType h).isProp
+            then
+              let appears ← args.mapM count_thms
+              let total := appears.foldl (fun s x => s+x) 0
+              if total = 0
+              then
+                return .some (n,((← inferType e), (← read)),[])
+              else
+                let r ← IO.rand 0 (total + 1)
+                if r = total + 1
+                then
+                  return .some (n,(← inferType e, (← read)),[])
+                else
+                  let mut sum := 0
+                  let mut idx := 0
+                  for w in appears do
+                    if r > sum then sum := sum + w ; idx := idx + 1
+                  let .some (N, goal, _) ← sample_goal_and_hyps (args.get! idx) | return .none
+                  let mut hyps := []
+                  let mut idx2 := 0
+                  for a in args do
+                    if idx2 ≠ idx
+                    then
+                      let .some (_,_, hyp) ← sample_goal_and_hyps a | pure ()
+                      hyps := hyp :: hyps
+                      idx2 := idx2 + 1
+                    else
+                      idx2 := idx2 + 1
+                  return .some (N, goal, hyps.join)
+            else
+              return .none
+      | _ => return .some (`Dummy ,(.const `DUMMY [], {}) ,[(← inferType e, (← read))])
 
+
+elab "testing_sample_goal_and_hyps" : command => do
+  let thm_name := `Nat.add_comm
+  let info := ((← getEnv).constants.find! thm_name)
+  match info with
+  | .thmInfo v =>
+      let .some (N,T,H) ← Elab.Command.liftTermElabM (sample_goal_and_hyps v.value) | throwError "aaahh 1"
+      --let TT := ← withLCtx T.2.lctx T.2.localInstances (inferType T.1)
+      let X ← Elab.Command.liftTermElabM  ((H.map Prod.fst).mapM (ppExpr) : MetaM (List Format))
+      IO.println s!"Sampleted: {N}\nGoal: {← Elab.Command.liftTermElabM  (ppExpr T.1)}\nAssumptions: {X}"
+  | _ => throwError "aaahh 2"
+
+testing_sample_goal_and_hyps
+
+#exit
 
 #check IO.rand
 #check Meta.transform
