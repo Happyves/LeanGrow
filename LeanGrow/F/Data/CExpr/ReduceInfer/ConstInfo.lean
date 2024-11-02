8e0d4ff8837ffe750b3472c273937c344bcaf8dc
Happyves <yves-j@gmx.de>
2024-11-02 16:42:35 +0100

    wöwjrk

--- LeanGrow/F/Data/CExpr/ReduceInfer/ConstInfo.lean
+++ LeanGrow/F/Data/CExpr/ReduceInfer/ConstInfo.lean
@@ -15,35 +15,57 @@ instance : BEq MatcherInfo where
   beq := fun ⟨a,b,c,d,e⟩ ⟨A,B,C,D,E⟩ =>
     (a == A) && (b == B) && (c == C) && (d == D) && (e == E)
 
-
 instance : Repr MatcherInfo where
   reprPrec := fun ⟨a,b,c,d,e⟩ _ =>
     "{" ++ s!"numParams := {repr a}, numDiscrs := {repr b}, altNumParams := {repr c}, uElimPos? := {repr d}, discrInfos := {repr e}" ++ "}"
 
+instance : BEq InductiveVal where
+  beq := fun ⟨a,b,c,d,e,f,g,h,i⟩ ⟨A,B,C,D,E,F,G,H,I⟩ =>
+    (a == A) && (b == B) && (c == C) && (d == D) && (e == E) && (f == F) && (g == G) && (h == H) && (i == I)
+
+
+instance : Repr InductiveVal where
+  reprPrec := fun ⟨_,b,c,d,e,f,g,h,i⟩ _ =>
+    "{" ++ s!"numParams := {repr b}, numIndices := {repr c}, all := {repr d}, ctors := {repr e}, isRec := {repr f}, isUnsafe := {repr g}, isReflexive := {repr h}, isNested := {repr i}" ++ "}"
+
+
+instance : BEq ConstructorVal where
+  beq := fun ⟨a,b,c,d,e,f⟩ ⟨A,B,C,D,E,F⟩ =>
+    (a == A) && (b == B) && (c == C) && (d == D) && (e == E) && (f == F)
+
+
+instance : Repr ConstructorVal where
+  reprPrec := fun ⟨_,b,c,d,e,f⟩ _ =>
+    "{" ++ s!"induct := {repr b}, cidx := {repr c}, numParams := {repr d}, numFields := {repr e}, isUnsafe := {repr f}" ++ "}"
+
+
 inductive CstInfo where
 | wVal (levelParams : List Lean.Name) (type : CExpr) (val : CExpr)
 | noVal (levelParams : List Lean.Name) (type : CExpr)
-| struc (levelParams : List Lean.Name) (type : CExpr) (ctorName : Lean.Name)
+| struc (levelParams : List Lean.Name) (type : CExpr) (ctorName : Lean.Name) (ctorVal : ConstructorVal)
 | mat (levelParams : List Lean.Name) (type : CExpr) (val : CExpr) (info : MatcherInfo)
+| indu (levelParams : List Lean.Name) (type : CExpr) (info : InductiveVal)
 deriving Inhabited, BEq, Repr
 
 def CstInfo.type : CstInfo → CExpr
   | .wVal _ t _ => t
   | .noVal _ t => t
-  | .struc _ t _ => t
+  | .struc _ t _ _=> t
   | .mat _ t _ _ => t
+  | .indu _ t _ => t
 
 def CstInfo.levelParams : CstInfo → List Lean.Name
   | .wVal t _ _ => t
   | .noVal t _ => t
-  | .struc t _ _ => t
+  | .struc t _ _ _ => t
   | .mat t _ _ _ => t
+  | .indu t _ _ => t
 
 
 /-
 Notes:
 
-- Don't forget to recognize structures when building the Trie
+- Don't forget to recognize structures when building the Trie (via !isStructureLike ?)
 - also get rid of annotations to account for Expr.consumeTypeAnnotations (in inferProj)
 
 
--- LeanGrow/F/Data/CExpr/ReduceInfer/Iota.lean
+++ LeanGrow/F/Data/CExpr/ReduceInfer/Iota.lean
@@ -85,3 +85,151 @@ def CExpr.reduceMatcher? (gnodeTypes : List (Array CExpr)) (gnodeTypesHandler : 
 #check (1+1)
 
 #check Subarray
+
+#check RecursorVal.k -- good docs
+
+
+def CExpr.mkNullaryCtor (cstData : CTrie CstInfo) (n : Name) (lvl : List Level) (as : List CExpr) (nparams : Nat) : Option CExpr :=
+  match cstData.find? n.toString with
+  | .some (.indu _ _ info) =>
+      let ct := info.ctors.head!
+      .some (CExpr.mkApp (.const ct lvl) (as.take nparams))
+  | _ => .none
+
+#eval Array.shrink #[1,2,3] 1
+
+
+def CExpr.toCtorWhenK (gnodeTypes : List (Array CExpr)) (gnodeTypesHandler : Nat → (Nat × Nat))
+  (ltxTypes : List (Array EmbedData)) (current : Option (Array EmbedData))
+  (cstData : CTrie CstInfo) (bvarCtx : List CExpr)
+  (recVal : RecursorVal) (major : CExpr) : CExpr :=
+  let majorType := cexprWhnf gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx
+    (cexprInferType gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx major)
+  let (H, AS) := CExpr.getApp majorType
+  match H with
+  | .const n lvl =>
+      if !(n == recVal.getInduct)
+      then
+        major
+      else
+        match CExpr.mkNullaryCtor cstData n lvl AS recVal.numParams with
+        | .none => major
+        | .some newCtorApp => newCtorApp
+  | _ => major
+
+
+
+def CExpr.toCtorIfLit : CExpr → CExpr
+  | .lit (.natVal v) =>
+    if v == 0 then .const `Nat.zero []
+    else .app (.const `Nat.succ []) (.lit (.natVal (v-1)))
+  -- can't be bothered lol ; goal would be `#eval toExpr "this".toList` ; who'll use strings anyway :>
+  -- | .lit (.strVal v) =>
+  --   .app (.const `String.mk []) (toExpr v.toList)
+  | e => e
+
+
+/-
+structure test where
+  a : Nat
+  b : Int
+
+#reduce test.a --fun self ↦ self.1
+-/
+
+def toCtorWhenStructure_help (strucName : Name) (nfields : Nat) (major : CExpr) (sofar : CExpr) : Nat → CExpr
+  | 0 => .app sofar (.proj strucName (nfields - 1) major)
+  | n+1 => toCtorWhenStructure_help strucName nfields major (.app sofar (.proj strucName (nfields - 1 - n) major) ) n
+
+
+
+def CExpr.toCtorWhenStructure (gnodeTypes : List (Array CExpr)) (gnodeTypesHandler : Nat → (Nat × Nat))
+  (ltxTypes : List (Array EmbedData)) (current : Option (Array EmbedData))
+  (cstData : CTrie CstInfo) (bvarCtx : List CExpr)
+  (strucName : Name) (major : CExpr) : CExpr :=
+  match cstData.find? strucName.toString with
+  | .some (.struc _ _ ctN ctV) =>
+      match (major.getApp).1 with
+      | .const N _ =>
+          if !(N == ctN)
+          then
+            major
+          else
+            let majorType := cexprInferType gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx major
+            let majorTypeType := cexprWhnf gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx
+              (cexprInferType gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx majorType)
+            if majorTypeType == .sort .zero
+            then
+              major
+            else
+              let (h,as) := major.getApp
+              match h with
+              | .const _ lvl =>
+                  let params := as.take ctV.numParams
+                  let wp := CExpr.mkApp (.const ctN lvl) params
+                  toCtorWhenStructure_help strucName ctV.numFields major wp ctV.numFields
+              | _ => major
+
+      | _ => major
+  | _ => major
+
+
+
+
+/-
+
+private def reduceRec (recVal : RecursorVal) (recLvls : List Level) (recArgs : Array Expr) (failK : Unit → MetaM α) (successK : Expr → MetaM α) : MetaM α :=
+  let majorIdx := recVal.getMajorIdx
+  if h : majorIdx < recArgs.size then do
+    let major := recArgs.get ⟨majorIdx, h⟩
+    let mut major ← if isWFRec recVal.name && (← getTransparency) == .default then
+      -- If recursor is `Acc.rec` or `WellFounded.rec` and transparency is default,
+      -- then we bump transparency to .all to make sure we can unfold defs defined by WellFounded recursion.
+      -- We use this trick because we abstract nested proofs occurring in definitions.
+      -- Alternative design: do not abstract nested proofs used to justify well-founded recursion.
+      withTransparency .all <| whnf major
+    else
+      whnf major
+    if recVal.k then
+      major ← toCtorWhenK recVal major
+    major := major.toCtorIfLit
+    major ← cleanupNatOffsetMajor major
+    major ← toCtorWhenStructure recVal.getInduct major
+    match getRecRuleFor recVal major with
+    | some rule =>
+      let majorArgs := major.getAppArgs
+      if recLvls.length != recVal.levelParams.length then
+        failK ()
+      else
+        let rhs := rule.rhs.instantiateLevelParams recVal.levelParams recLvls
+        -- Apply parameters, motives and minor premises from recursor application.
+        let rhs := mkAppRange rhs 0 (recVal.numParams+recVal.numMotives+recVal.numMinors) recArgs
+        /- The number of parameters in the constructor is not necessarily
+           equal to the number of parameters in the recursor when we have
+           nested inductive types. -/
+        let nparams := majorArgs.size - rule.nfields
+        let rhs := mkAppRange rhs nparams majorArgs.size majorArgs
+        let rhs := mkAppRange rhs (majorIdx + 1) recArgs.size recArgs
+        successK rhs
+    | none => failK ()
+  else
+    failK ()
+-/
+
+
+
+def CExpr.reduceRec  (gnodeTypes : List (Array CExpr)) (gnodeTypesHandler : Nat → (Nat × Nat))
+  (ltxTypes : List (Array EmbedData)) (current : Option (Array EmbedData))
+  (cstData : CTrie CstInfo) (bvarCtx : List CExpr)
+  (recVal : RecursorVal) (recLvls : List Level) (recArgs : List Expr) (on : CExpr) : CExpr :=
+  let majorIdx := recVal.getMajorIdx
+  if h : majorIdx < recArgs.length
+  then
+    let major := cexprWhnf gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx (recArgs.get ⟨majorIdx, h⟩)
+    let eta_1 := if recVal.k then (CExpr.toCtorWhenK gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx recVal major) else major
+    let eta_2 := eta_1.toCtorIfLit
+    let eta_3 := CExpr.toCtorWhenStructure gnodeTypes gnodeTypesHandler ltxTypes current cstData bvarCtx
+      recVal.getInduct eta_2
+    sorry
+
+#check getRecRuleFor
