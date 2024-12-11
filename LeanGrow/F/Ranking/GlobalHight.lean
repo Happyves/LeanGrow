eaa01a0ae76d94304b6567cea29ecf790a243744
Happyves <yves-j@gmx.de>
2024-12-11 17:03:12 +0100

    rhgfwpg

--- LeanGrow/F/Ranking/GlobalHight.lean
+++ LeanGrow/F/Ranking/GlobalHight.lean
@@ -73,7 +73,7 @@ private partial def step (env : Environment)
                 then
                   match more with
                   | s :: next =>
-                      let nMxs := UpdateMax 1 hc
+                      let nMxs := UpdateMax 0 hc
                       step env ((heights.sorted_insert nx.toString 0).sorted_insert s.toString (nMxs.headD 0)) nArgNum nMxs.tail next
                   | [] => {}
                 else
@@ -91,7 +91,7 @@ private partial def step (env : Environment)
                         then
                           match more with
                           | s :: next =>
-                              let nMxs := UpdateMax (H+1) hc
+                              let nMxs := UpdateMax H hc
                               step env ((heights.sorted_insert nx.toString H).sorted_insert s.toString (nMxs.headD 0)) nArgNum nMxs.tail next
                           | [] => {}
                         else
@@ -108,9 +108,12 @@ private partial def step (env : Environment)
                         else
                           step env (heights.sorted_insert nx.toString 0) nArgNum (updateMax 0 hc) more
 
+
 #check List.append_assoc
 
 #check Nat.add_comm
+
+#check List.exists_mem_cons_of_exists
 
 private def test : CoreM Unit := do
   let res := step (← getEnv) {} [] [] [`List.append_assoc] --[`Nat.mul_le_mul]
@@ -134,3 +137,77 @@ private def test2 : CoreM Unit := do
   IO.print (CTrie.toList (build_height_weights (← getEnv) `Mathlib.Data.List))
 
 -- #eval test2
+-- takes ages, overflows...
+
+
+def sanity_check (env : Environment) (N : Name) : List Name :=
+  let modules := env.header.moduleNames.map (N.isPrefixOf ·)
+  env.constants.map₁.fold (fun T n info =>
+    if modules[env.const2ModIdx[n].get! (α := Nat)]!
+    then
+      if ConstantInfo.isThmOrAx info
+      then n :: T
+      else T
+    else T
+    ) {}
+
+private def test3 : CoreM Unit := do
+  IO.print ((sanity_check (← getEnv) `Mathlib.Data.List).take 20)
+
+-- #eval test3
+
+
+def goodLord (env : Environment) (N : Name) : CoreM Unit := do
+  let rec iter (T : CTrie Nat) : List Name → CTrie Nat
+    | [] => T
+    | n :: more => dbg_trace s!"\nLOOKING at {n}\n"; iter (step env T [] [] [n]) more
+  let namez := (sanity_check (← getEnv) N).take 20
+  let res := iter {} namez
+  let sanity_1 := res.sorted_find? "List.get_reverse"
+  --let sanity_2 := res.sorted_find? "List.getElem?_indexOf"
+  IO.print s!"done {sanity_1} "-- {sanity_2}
+
+private def test4 : CoreM Unit := do
+  (goodLord (← getEnv) `Mathlib.Data.List)
+
+-- #eval test4
+
+
+private inductive Instr where
+| comp (_ : Name)
+| store (_ : Name) (_ : Nat)
+
+
+private partial def step2 (env : Environment) (heights : CTrie Nat) (n : Name) : CTrie Nat :=
+  let rec go (heights : CTrie Nat) (stack : List Nat) : List Instr → CTrie Nat
+    | .comp n :: more => --dbg_trace s!"comp {n} ; stack {stack}"
+        match heights.sorted_find? n.toString with
+        | .some H => go heights (H :: stack) more
+        | .none =>
+            match (env.constants.find! n).value? with
+            | .none => go (heights.sorted_insert n.toString 0) (0 :: stack) more
+            | .some V =>
+                let csts := (Expr.getConstNamesF V).dedup.filter (fun x => ConstantInfo.isThmOrAx (env.constants.find! x))
+                match csts with
+                | [] => go (heights.sorted_insert n.toString 0) (0 :: stack) more
+                | _ => go heights stack ((csts.map Instr.comp) ++ ( Instr.store n csts.length :: more))
+    | .store n as :: more => --dbg_trace s!"store {n} {as} ; stack {stack}"
+        match (stack.take as).maximum? with
+        | .none => {} -- shouldn't
+        | .some H => go (heights.sorted_insert n.toString (H+1)) ((H+1) :: (stack.drop as)) more
+    | [] => heights
+  go heights [] [.comp n]
+
+
+
+#check List.append_assoc
+
+#check Nat.add_comm
+
+#check List.exists_mem_cons_of_exists
+
+private def test_s1 : CoreM Unit := do
+  let res := step2 (← getEnv) {} `List.get_reverse
+  IO.print (CTrie.toList res)
+
+-- #eval test_s1
