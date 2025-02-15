8ffec3f094e9e20dd0d9e48b19a09918e77553b3
Happyves <yves-j@gmx.de>
2025-02-15 12:34:24 +0100

    wrrpgijbwr

--- LeanGrow/F/EnvironmentManagement/Imports/Study.lean
+++ LeanGrow/F/EnvironmentManagement/Imports/Study.lean
@@ -161,10 +161,154 @@ elab "readOlean?" : command => do
 
 
 #check_failure Environment.mk --is private :<
-
+-- potential hack might be to add a fake import and modify the environement via
+#check finalizeImport
 
 readOlean?
 
 #check myConst
 --#eval myConst --fail
 #reduce myConst
+
+
+#check Environment.extensions
+#check EnvExtensionState
+
+#check ConstantInfo.rec
+
+elab "studyExtraNames" n:name : command => do
+  let mFile ← findOLean n.getName
+  let (data,_) ← readModuleData mFile
+  IO.println data.extraConstNames
+
+studyExtraNames `Mathlib.Data.Nat.Defs
+
+studyExtraNames `Init.Prelude
+
+#check_failure Nat.decLt._boxed
+-- in Init, so module is imported
+
+#check persistentEnvExtensionsRef.get
+#check PersistentEnvExtension.name
+
+
+#eval (do let res ← persistentEnvExtensionsRef.get ; IO.println (res.map PersistentEnvExtension.name) : IO Unit)
+
+#check addDecl
+
+#check Environment.addDecl
+
+
+elab "com" : command => do
+  let exts ← persistentEnvExtensionsRef.get
+  let names := String.intercalate "\n"
+    (exts.map (fun x => (PersistentEnvExtension.name x).toString)).toList
+  let loc := "/home/yves/Desktop/CodeWorkspace/Lean4_General/LeanGrow/LeanGrow/F/EnvironmentManagement/Imports/testin.txt"
+  IO.FS.writeFile ⟨loc⟩ names
+
+--com
+
+elab "com2" : command => do
+  let env ← getEnv
+  let exts ← persistentEnvExtensionsRef.get
+  let names := String.intercalate "\n"
+    (exts.map (fun x => (PersistentEnvExtension.name x).toString)).toList
+  let dec := Declaration.defnDecl (mkDefinitionValEx `LG.extract [] (.const `String []) (Expr.lit (Literal.strVal names)) .opaque .safe [])
+  let nenv := env.addDecl {} dec
+  match nenv with
+  | .ok yes => setEnv yes
+  | _ => pure ()
+
+#check Expr.lit
+#check Literal.strVal
+#check mkDefinitionValEx
+#check String.intercalate
+
+def fakeInstructions :=
+"
+import Lean
+
+open Lean
+
+elab \"com\" : command => do
+  let exts ← persistentEnvExtensionsRef.get
+  let names := String.intercalate \"\\n\"
+    (exts.map (fun x => (PersistentEnvExtension.name x).toString)).toList
+  let loc := \"/home/yves/Desktop/CodeWorkspace/Lean4_General/LeanGrow/LeanGrow/F/EnvironmentManagement/Imports/testin.txt\"
+  IO.FS.writeFile ⟨loc⟩ names
+
+com
+"
+
+
+
+
+elab "studyPersEnvExt" n:name : command => do
+  let fakeFile := "import " ++ n.getName.toString ++ fakeInstructions
+  let (_,_) ← runFrontend fakeFile {} "nope" `nope
+
+-- studyPersEnvExt `Mathlib.Data.Nat.Defs
+-- studyPersEnvExt `Init.Prelude
+-- work, and carry out IO, but strangely also adds the Lake stuff, which doesn't happen
+-- if we remove the lake import in this file, so it seems that environements aren't loaded
+-- the way I imagined them ?
+
+
+#check expandInitialize
+
+
+#check importModules
+#check importModulesCore
+#check finalizeImport
+
+#check 1
+
+/-
+The way imports seem to work is by starting wih a list of `Import`s, which are just names.
+`importModules` is just the top function: it sets flags, and calls `importModulesCore` and `finalizeImport`.
+`importModulesCore` adds the name of the import to the list of import names, uses `findOLean` to find the
+corresponding olean filepath, uses `readModuleData` to get the module data, and first makes a recursive call
+on the imports of that imported module. When the call returns, the module data of the current module is
+pushed onto stack of the ImportState.
+`finalizeImport` will turn these stacks of data into an `Environment`. Most notably, it builds the constant
+hashmaps by folding over the module datas, checking if names clash.
+
+-/
+
+
+
+inductive Tree where
+| leaf (_ : Name)  | node (_ : Name) (kist : Array Tree)
+deriving Inhabited, Repr, BEq
+
+/-
+Compare with:
+- mathlib > shake > main
+- importGraphs > ImportGraphs > Import
+-/
+
+-- hopefully no cycles
+partial def goImp (i : Import) : IO Tree := do
+  let mFile ← findOLean i.module
+  let (data,_) ← readModuleData mFile
+  let is := data.imports
+  if is.isEmpty
+  then
+    return .leaf i.module
+  else
+    let Ts ← is.mapM goImp
+    return .node i.module Ts
+
+
+
+
+elab "getImportTree" n:name : command => do
+  let mFile ← findOLean n.getName
+  let (data,_) ← readModuleData mFile
+  let is := data.imports
+  let Ts ← (is.mapM goImp : IO _)
+  let res := Tree.node n.getName Ts
+  IO.println (repr res)
+  
+-- execution caused a complete freeze XD
+
